d55abd65ff0dea49b8e9acaad4b38dab9ab18d83
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-01-11 15:53:20 +0100

    add to api; new pipelines; pear policies

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,5 @@
 const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const darkCodeTheme = require("prism-react-renderer/themes/oceanicNext");
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
